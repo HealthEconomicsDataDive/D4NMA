53beea8a92c9f9fe12e152935b7799e43f0e76e5
Bogdasayen <howard.thom@bristol.ac.uk>
2020-01-22 12:16:46 +0000

    Corrected errors

--- analyse_data_1.R
+++ analyse_data_1.R
@@ -1,8 +1,8 @@
 # Script to run the SSI ICL NMA once the formatted data are loaded
 # Belfast data dive 21/22 Jan 2020
 
-baseline_directory<-"~/Bristol/R for CEA/R data dive Belfast 2020/NMA data project"
-setwd(baseline_directory)
+#baseline_directory<-"~/Bristol/R for CEA/R data dive Belfast 2020/NMA data project"
+#setwd(baseline_directory)
 
 source("model_binomial_logistic_1.R")
 
@@ -21,7 +21,7 @@ inits2<-list(d=c(NA,rep(-1,bugs_data$nt-1)),mu=rep(0.5,bugs_data$ns),sd=0.5,B=-0
 bugs_inits<-list(inits1,inits2)
 
 # Random effects network meta-analysis
-bugs_object_re<-bugs(data = bugs_data, inits = bugs_inits_re, parameters.to.save = c("d","totresdev"), 
+bugs_object_re<-bugs(data = bugs_data, inits = bugs_inits, parameters.to.save = c("d","totresdev"), 
                      model = model_binomial_logistic_re, clearWD = TRUE, summary.only = FALSE, 
                      n.iter = (num_sims + burn_in), n.burnin = burn_in, n.chains = n_chains, bugs.seed = 1, 
                      debug = TRUE)
